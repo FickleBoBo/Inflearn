952e75387da4a672e62e3757479f00e896a88811
FickleBoBo <yukmw0704@gmail.com>
2024-06-17 06:36:16 +0900

    김영한의 실전 자바 2편 제네릭 학습

---
+++ Java_04. 김영한의 실전 자바 - 중급 2편/project/java-mid2/src/generic/test/ex3/UnitPrinter.java
@@ -0,0 +1,14 @@
+package generic.test.ex3;
+
+import generic.test.ex3.unit.BioUnit;
+
+public class UnitPrinter {
+
+    static <T extends BioUnit> void printV1(Shuttle<T> shuttle){
+        System.out.println("이름: " + shuttle.out().getName() + ", HP: " + shuttle.out().getHp());
+    }
+
+    static void printV2(Shuttle<? extends BioUnit> shuttle){
+        System.out.println("이름: " + shuttle.out().getName() + ", HP: " + shuttle.out().getHp());
+    }
+}
